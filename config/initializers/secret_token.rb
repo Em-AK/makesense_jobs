36ddae2138a31b3fc07001658014dfef9ec32fa5
em-ak <emilien.ak@gmail.com>
2014-01-25 23:23:21 +0100

    Big Bang commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Makesensejobs::Application.config.secret_key_base = 'd5338f76bacd57bd0b5561783a8a0246859291303bee273856455ae28c3c762c153dfeaf0e9733ef9b340db256554986b6bf730a065581fd947e8cc01d6abf61'
