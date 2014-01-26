14fd282b00b15e4efc1498a615e49e9abaf163f4
em-ak <emilien.ak@gmail.com>
2014-01-26 16:28:54 +0100

    Create company model

---
+++ db/migrate/20140126152442_create_companies.rb
@@ -0,0 +1,11 @@
+class CreateCompanies < ActiveRecord::Migration
+  def change
+    create_table :companies do |t|
+      t.string :name
+      t.string :website
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140126152442) do
+
+  create_table "companies", force: true do |t|
+    t.string   "name"
+    t.string   "website"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
