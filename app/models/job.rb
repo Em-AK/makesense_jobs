59627563b20d0a7ca817e5100986077fb04804fc
em-ak <emilien.ak@gmail.com>
2014-03-07 11:41:06 +0100

    Validates url and presence of apply info #22

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,9 +2,18 @@ class Job < ActiveRecord::Base
   validates :email, presence: true
   validates :company_name, presence: true
   validates :title, presence: true
+  validates :apply, presence: true
 
   mount_uploader :logo, LogoUploader
+
   before_create :add_token
+  before_save :validate_url
+
+  def validate_url 
+    unless !self.company_url || self.company_url[0..6] == "http://"
+      self.company_url = "http://#{self.company_url}"
+    end
+  end
 
   private
 
