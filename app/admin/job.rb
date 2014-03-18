b3e936157288966cad4be955440325d6981eaab7
em-ak <emilien.ak@gmail.com>
2014-03-19 00:09:54 +0100

    Refactor admin job show

--- app/admin/job.rb
+++ app/admin/job.rb
@@ -14,5 +14,32 @@ permit_params :title, :description, :apply, :location, :company_name, :company_u
     column :updated_at
     default_actions
   end
+
+  #GET /admin/jobs/:id
+  show do |job|
+    attributes_table do
+      row :id
+      row :title
+      row :company_name
+      row :company_url do
+        link_to job.company_url, job.company_url
+      end
+      row :logo do
+        image = image_tag job.logo.url
+        link_to image, job.logo.url if job.logo.url
+      end
+      row :location
+      row :description
+      row :apply
+      row :created_at
+      row :updated_at
+      row :email
+      row :token do
+        link_to job.token, new_job_path(token: job.token)
+      end
+      row :published
+      row :highlighted
+    end
+  end
   
 end
