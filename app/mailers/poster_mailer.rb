5483da35c8c27519340f14fdc259b02709b38c2a
em-ak <emilien.ak@gmail.com>
2014-04-02 16:38:07 +0200

    Fix mailer host urls

--- app/mailers/poster_mailer.rb
+++ app/mailers/poster_mailer.rb
@@ -4,8 +4,8 @@ class PosterMailer < ActionMailer::Base
   def new_job_email(job)
     @recruiter = job.email
     @title = job.title
-    @job_url = job_url(job, host: "http://jobs.makesense.org/")
-    @edit_url = new_job_url(token: job.token, host: "http://jobs.makesense.org/")
+    @job_url = job_url(job, host: "http://jobs.makesense.org")
+    @edit_url = new_job_url(token: job.token, host: "http://jobs.makesense.org")
     @company = job.company_name
 
     mail(to: @recruiter, bcc: 'emilien.ak@gmail.com' , subject: "[Jobs.MakeSense] #{job.title}")
